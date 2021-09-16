adb7146f998fac43729eb2fd53d2f277c29a94cb
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-09-16 16:30:24 -0500

    Ciclo3

--- P32/Semana3/dev/calculadora/src/components/CalculadoraBasica.vue
+++ P32/Semana3/dev/calculadora/src/components/CalculadoraBasica.vue
@@ -1,28 +1,53 @@
-<template>
-  <div>
+  <template>
+  <v-container>
     <titulo />
-    <h1>Calculadora Básica</h1>
-    <input type="number" v-model="n1" />
-    <br />
-    <input type="number" v-model="n2" />
-    <br />
-    <v-btn
-      color="primary"
-      elevation="15"
-      rounded
-      x-large
-      @click="suma()"
-    >Sumar</v-btn>
-    <v-btn
-      color="primary"
-      elevation="15"
-      rounded
-      x-large
-      @click="resta()"
-    >Restar</v-btn>
+    <!-- Componente "Titulo.vue" -->
+    <v-row>
+      <v-col class="text-center">
+        <h1>Calculadora Básica</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field
+          type="number"
+          label="Número 1"
+          :rules="rules"
+          hide-details="auto"
+          v-model="n1"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+          type="number"
+          label="Número 2"
+          :rules="rules"
+          hide-details="auto"
+          v-model="n2"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col>
+        <v-btn color="primary" elevation="15" rounded x-large @click="sumar()"
+          >Sumar</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn color="primary" elevation="15" rounded x-large @click="restar()"
+          >Restar</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn color="primary" elevation="15" rounded x-large @click="multiplicar()"
+          >Multiplicar</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn color="primary" elevation="15" rounded x-large @click="dividir()"
+          >Dividir</v-btn>
+      </v-col>
+    </v-row>
     <br />
     {{ resp }}
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -38,18 +63,32 @@ export default {
       n1: null,
       n2: null,
       resp: "",
+      rules: [
+        value => !!value || 'Requerido.',
+        value => (value && value.length >= 3) || 'Min 3 caracteres',
+      ],
     };
   },
   methods: {
-    suma() {
+    sumar() {
       this.resp = parseFloat(this.n1) + parseFloat(this.n2);
     },
-    resta() {
+    restar() {
       this.resp = parseFloat(this.n1) - parseFloat(this.n2);
+    },
+    multiplicar() {
+      this.resp = parseFloat(this.n1) * parseFloat(this.n2);
+    },
+    dividir() {
+      if (this.n2 == 0) {
+        alert("Not a number");
+      } else {
+        this.resp = parseFloat(this.n1) / parseFloat(this.n2);
+      }
     },
   },
 };
 </script>
 
-<style>
+  <style>
 </style>
--- P32/Semana3/dev/calculadora/src/components/Titulo.vue
+++ P32/Semana3/dev/calculadora/src/components/Titulo.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim nihil illum
-      hic commodi omnis incidunt quod animi necessitatibus ipsum voluptas
-      corporis, dolores rem et tenetur ea eaque quas! Dolor, sint!
+  <v-container>
+    <p class="text-center">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim nihil illum
+    hic commodi omnis incidunt quod animi necessitatibus ipsum voluptas
+    corporis, dolores rem et tenetur ea eaque quas! Dolor, sint!
+    <br />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat qui magni
+    quod dolor debitis tempora quas cum quibusdam perspiciatis aspernatur, quae
+    numquam accusantium quo cupiditate, ut eius ipsam voluptatibus laboriosam?
     </p>
-  </div>
+  </v-container>
 </template>
 
 <script>
