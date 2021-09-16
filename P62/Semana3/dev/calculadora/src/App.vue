ecfbeda60b5f891b1f282fd7a2f6faf77692f7ab
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-09-16 14:08:49 -0500

    Ciclo3

--- P62/Semana3/dev/calculadora/src/App.vue
+++ P62/Semana3/dev/calculadora/src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <v-app>
-    <v-main>
-      <calculadora-basica />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import CalculadoraBasica from './components/CalculadoraBasica.vue';
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
+#nav {
+  padding: 30px;
 
-  components: {
-    CalculadoraBasica
-  },
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  data: () => ({
-    //
-  }),
-};
-</script>
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
