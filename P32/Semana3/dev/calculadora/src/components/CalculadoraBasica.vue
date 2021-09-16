cdc4a156ccb1c50b2949e7031c091b3dee576e08
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-09-16 07:12:07 -0500

    Ciclo3

--- P32/Semana3/dev/calculadora/src/components/CalculadoraBasica.vue
+++ P32/Semana3/dev/calculadora/src/components/CalculadoraBasica.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-      <h1>Calculadora Básica</h1>
+    <titulo />
+    <h1>Calculadora Básica</h1>
+    <input type="number" v-model="n1" />
+    <br />
+    <input type="number" v-model="n2" />
+    <br />
+    <button @click="suma()">Sumar</button>
+    <button @click="resta()">Restar</button>
+    <br />
+    {{ resp }}
   </div>
 </template>
 
 <script>
+import Titulo from "./Titulo.vue";
+
 export default {
-    
-}
+  name: "CalculadoraBasica",
+  components: {
+    Titulo,
+  },
+  data: () => {
+    return {
+      n1: null,
+      n2: null,
+      resp: "",
+    };
+  },
+  methods: {
+    suma() {
+      this.resp = parseFloat(this.n1) + parseFloat(this.n2);
+    },
+    resta() {
+      this.resp = parseFloat(this.n1) - parseFloat(this.n2);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ P32/Semana3/dev/calculadora/src/components/Titulo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim nihil illum
+      hic commodi omnis incidunt quod animi necessitatibus ipsum voluptas
+      corporis, dolores rem et tenetur ea eaque quas! Dolor, sint!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
