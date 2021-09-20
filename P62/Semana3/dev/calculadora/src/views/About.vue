935e1dcfd3a4c14441bfeecfb287cc8b2c283ae8
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-09-20 10:36:42 -0500

    Ciclo3

--- P62/Semana3/dev/calculadora/src/views/About.vue
+++ P62/Semana3/dev/calculadora/src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h2>About</h2>
+    <calculadora-basica />
   </div>
 </template>
+
+<script>
+import CalculadoraBasica from "../components/CalculadoraBasica.vue";
+
+export default {
+  components: {
+    CalculadoraBasica,
+  },
+};
+</script>
+
+<style>
+</style>
