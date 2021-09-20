25634e2b0f9c38cb3b30ac585eb5ecac5fd04b7e
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-09-20 08:51:47 -0500

    Ciclo3

---
+++ P2/Semana3/dev/calculadora/src/components/Personajes.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <h1>Personajes</h1>
+      <ul>
+          <li v-for="item in personajes" :key="item.Name">
+              {{ item.Name }} - {{ item.Planet }}
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import store from "../store/index.js";
+
+export default {
+    data: () => {
+        return {};
+    },
+    methods: {},
+    created: () => {
+        //accede a las acciones del store
+        store.dispatch("getPersonajes");
+    },
+    computed: {
+        personajes: () => {
+            return store.state.personajes;
+        }
+    }
+}
+</script>
+
+<style>
+</style>
--- P2/Semana3/dev/calculadora/src/router/index.js
+++ P2/Semana3/dev/calculadora/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Personajes from '../views/Personajes.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/personajes',
+    name: 'Personajes',
+    component: Personajes
   }
 ]
 
