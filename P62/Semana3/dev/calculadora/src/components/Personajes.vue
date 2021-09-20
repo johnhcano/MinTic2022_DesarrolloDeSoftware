412b3b5d4326c90e5c78f177913e901dbf300d3d
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-09-20 11:55:31 -0500

    Ciclo3

--- P62/Semana3/dev/calculadora/src/components/Personajes.vue
+++ P62/Semana3/dev/calculadora/src/components/Personajes.vue
@@ -1,13 +1,34 @@
 <template>
-  <h1>Personajes</h1>
+  <div>
+    <h1>Personajes</h1>
+    <ul>
+      <li v-for="item in personajes" :key="item.Name">
+          {{ item.Name }} - {{ item.Age }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
+import store from "../store/index.js";
 
-}
+export default {
+    data: () => {
+        return {};
+    },
+    methods: {},
+    created: () => {
+        //accede a las acciones del store
+        store.dispatch("getPersonajes");
+    },
+    computed: {
+        personajes: () => {
+            return store.state.personajes;
+        }
+    }
+
+};
 </script>
 
 <style>
-
 </style>
