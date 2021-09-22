a3f7865c9fae8856448e1dd17ac5c6bf4d3b759f
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-09-22 18:38:49 -0500

    Ciclo3

--- P32/Semana3/dev/calculadora/src/components/Personajes.vue
+++ P32/Semana3/dev/calculadora/src/components/Personajes.vue
@@ -1,10 +1,31 @@
 <template>
-  <h2>Personajes Futurama</h2>
+  <div>
+    <h2>Personajes Futurama</h2>
+    <ul>
+      <li v-for="item in personajes" :key="item.Name">
+        {{ item.Name }} - {{ item.Planet}} - {{ item.Status }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
+import store from "../store/index.js";
 
+export default {
+  data: () => {
+    return {};
+  },
+  methods: {},
+  created: () => {
+    //Acceder a las actions del store
+    store.dispatch("getPersonajes");
+  },
+  computed: {
+    personajes: () => {
+      return store.state.personajes;
+    }
+  }
 }
 </script>
 
--- P32/Semana3/dev/calculadora/src/store/index.js
+++ P32/Semana3/dev/calculadora/src/store/index.js
@@ -5,10 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    personajes: []
   },
   mutations: {
+    setPersonajes(state, payload){
+      state.personajes = payload;
+    }
   },
   actions: {
+    async getPersonajes({commit}){
+      const peticion = await fetch('https://futuramaapi.herokuapp.com/api/v2/characters');
+      const data = await peticion.json();
+      console.log(data);
+      commit('setPersonajes', data);
+    }
   },
   modules: {
   }
