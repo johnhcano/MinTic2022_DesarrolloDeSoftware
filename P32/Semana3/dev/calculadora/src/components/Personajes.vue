4744430c9d360ebe2700eb4308ec210f8475446f
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-09-30 16:46:51 -0500

    Ciclo3

--- P32/Semana3/dev/calculadora/src/components/Personajes.vue
+++ P32/Semana3/dev/calculadora/src/components/Personajes.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
-    <h2>Personajes Futurama</h2>
-    <ul>
-<!--      
+    <!--    <h2>Personajes Futurama</h2>-->
+    <h2>Arqueros Selección Colombia de los 90's</h2>
+
+    <!--      
       <li v-for="item in personajes" :key="item.Name"> 
         {{ item.Name }} - {{ item.Planet}} - {{ item.Status }}
       </li>
 -->
-      <li v-for="item in personajes" :key="item.id"> 
-        {{ item.nombre }} {{ item.apellido}}
-      </li>
-    </ul>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Nombre</th>
+            <th class="text-left">Apellido</th>
+            <th class="text-left">Edad</th>
+            <th class="text-left">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in personajes" :key="item.id">
+            <td>{{ item.nombre }}</td>
+            <td>{{ item.apellido }}</td>
+            <td>{{ item.edad }}</td>
+            <td>{{ item.email }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </div>
 </template>
 
@@ -29,11 +46,10 @@ export default {
   computed: {
     personajes: () => {
       return store.state.personajes;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
